479f0e6b869b595d250e409090e7c71f0300b8cf
Jonathan Kittell <jonathan.kittell@vecima.com>
2023-12-01 14:11:12 -0500

    remove file

--- streaminfo.go
+++
@@ -1,8 +0,0 @@
-package mediastreaminfo
-
-import "gopkg.in/vansante/go-ffprobe.v2"
-
-type StreamInfo struct {
-	Name    string            `json:"name"`
-	Ffprobe ffprobe.ProbeData `json:"ffprobe"`
-}
